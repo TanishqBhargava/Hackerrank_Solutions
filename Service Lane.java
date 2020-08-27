1f0ce435ad71b044397ad02a7f3e866b044bd8a1
TanishqBhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-08-27 21:43:24 +0530

    Create Service Lane.java

---
+++ Service Lane.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int T = scan.nextInt();
+        byte[] widths = new byte[n];
+        for (int i = 0; i < n; ++i) {
+            widths[i] = scan.nextByte();
+        }
+
+        while (T-- > 0) {
+            int i = scan.nextInt();
+            int j = scan.nextInt();
+
+            byte min = 3;
+            for (int k = i; k <= j; ++k) {
+                min = (byte) Math.min(widths[k], min);
+                if (min == 1) {
+                    break;
+                }
+            }
+            System.out.println(min);
+        }
+    }
+}

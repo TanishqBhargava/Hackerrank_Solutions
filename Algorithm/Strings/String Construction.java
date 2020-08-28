53f4ccce42d986e6813571125a357b88c3407dcc
TanishqBhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-08-28 21:47:31 +0530

    Create String Construction.java

---
+++ Algorithm/Strings/String Construction.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.util.*; 
+import java.text.*; 
+import java.math.*; 
+import java.util.regex.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    for(int a0 = 0; a0 < n; a0++){
+        String s = in.next();
+        System.out.println(s.chars().distinct().count());
+    }
+}
+}

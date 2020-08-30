f1e858dc89edcf3809dde19817829edafb908eef
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-08-30 08:34:55 +0530

    Create Diwali Lights.cpp

---
+++ Mathematics/Fundamentals/Diwali Lights.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int n,ans=1;
+        cin>>n;
+        for(int i=0;i<n;i++)
+        {
+            ans=ans*2;
+            ans=ans%100000;
+        }
+        cout<<ans-1<<endl;
+    }
+    
+}

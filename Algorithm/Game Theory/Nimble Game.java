a34ea9f9240d88ce945c2bb5fe18a227cacc7b7f
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-08-30 14:43:16 +0530

    Create Nimble Game.java

---
+++ Algorithm/Game Theory/Nimble Game.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Nimble {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int r = 0;
+            for (int i = 0; i < n; i++) 
+                if (scanner.nextInt() % 2 == 1)
+                    r ^= i;
+            if (r == 0)
+                System.out.println("Second");
+            else
+                System.out.println("First");
+        }
+    }
+}

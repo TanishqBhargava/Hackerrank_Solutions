28f0ced8aec2235627d431085cde9ff0ade7cd7f
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-09-04 19:26:26 +0530

    Create Functions.cpp

---
+++ C++/Introduction/Functions.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d)
+{
+    return max(max(max(a,b),c),d);
+}
+int main() {
+    int a, b, c, d;
+    cin>>a>>b>>c>>d;
+    int ans = max_of_four(a, b, c, d);
+    cout<<ans;
+    return 0;
+}

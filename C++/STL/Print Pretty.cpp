50012d782f31349c4c0515302e06f54b1c8fd101
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-09-09 18:52:17 +0530

    Update Print Pretty.cpp

--- C++/STL/Print Pretty.cpp
+++ C++/STL/Print Pretty.cpp
@@ -1,7 +1,28 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main()
+{
+    int T; cin >> T;
+    cout << setiosflags(ios::uppercase);
+    cout << setw(0xf) << internal;
+    while(T--) 
+    {
+        double A; cin >> A;
+        double B; cin >> B;
+        double C; cin >> C;
+
+        /* BEGIN: unlocked */
+          // Sample solution:
             cout << left << hex << showbase << nouppercase << (long long) A << endl;
+
             // One decimal place, right aligned, show sign, and fill left with underscores
-            cout.precision(2);
-            cout << showpos << fixed << setfill('_') << setw(15) << right << B << endl;
+            cout << showpos << fixed << setfill('_') << setw(15) << right << setprecision(2) << B << endl;
+
             // Scientific notation with 9 decimal places
-            cout.precision(9);
-            cout << noshowpos << uppercase << scientific << C << endl;
+            cout << noshowpos << uppercase << scientific << setprecision(9) C << endl;
+        /* END: unlocked */
+    }
+    return 0;
+}

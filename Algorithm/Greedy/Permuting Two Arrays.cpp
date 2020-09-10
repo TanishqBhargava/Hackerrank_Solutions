04e9a15db738a45c9bd9a92bdb9c436379ec5ab8
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-09-10 20:02:44 +0530

    Create Permuting Two Arrays.cpp

---
+++ Algorithm/Greedy/Permuting Two Arrays.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve() 
+{
+    int N, K;
+    cin >> N >> K;
+    vector<int> a(N), b(N);
+    for (int i = 0 ; i < N; ++i) 
+        cin >> a[i];
+    for (int i =0 ; i < N; ++i) 
+        cin >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end(), greater<int>());
+    for (int i = 0; i < N; ++i) 
+        if (a[i] + b[i] < K) 
+        return false;
+    return true;
+}
+
+int main() 
+{
+    int t; 
+    cin >> t;
+    for (int i =0; i < t; ++i) 
+        cout << (solve() ? "YES" : "NO") << endl;
+}

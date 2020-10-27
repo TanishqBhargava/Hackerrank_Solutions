d7b0262091b14c8f217c96a2104d008ad54f2eb7
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2020-10-27 11:29:31 +0530

    Update 2D Array - DS.cpp

--- Data Structures/Arrays/2D Array - DS.cpp
+++ Data Structures/Arrays/2D Array - DS.cpp
@@ -8,20 +8,20 @@ using namespace std;
 
 int main() 
 {
-    int a[6][6],s;
+    int arr[6][6],s;
     int m = INT_MIN;
     for(int i = 0; i < 6; i++)
     {
         for(int j = 0; j < 6; j++)
         {
-            cin >> a[i][j];
+            cin >> arr[i][j];
         }
     }
     for(int i = 0; i < 4; i++)
     {    
         for(int j = 0; j < 4; j++)
         {
-            s = (a[i][j] + a[i][j + 1] + a[i][j + 2] + a[i + 1][j + 1] + a[i + 2][j] + a[i + 2][j + 1] + a[i + 2][j + 2]);
+            s = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]);
             if(s > m)
                 m = s;
         }    
